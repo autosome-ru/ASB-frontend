7f0978755e9a08ecf71b5aa843b904613d672ff2
M1kss <boytsovs.av@phystech.edu>
2020-03-29 19:29:50 +0300

    fix url

--- src/app/models/urls.ts
+++ src/app/models/urls.ts
@@ -1,4 +1,4 @@
-const serverUrl = "http://localhost:5000" + "/api/v1";
+const serverUrl = "api/v1";
 export const snpsInfoUrl = serverUrl + "/snps";
 export const searchOptionsUrl = (tfOrcl: "tf" | "cl") => serverUrl + "/search/" + tfOrcl + "/hint";
 export const searchSnpsResultsUrl = serverUrl + "/search/snps";
