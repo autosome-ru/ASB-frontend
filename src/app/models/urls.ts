fe4641ba45705207ff0c2938ee26bca33bfdec81
serj <aswq22013@gmail.com>
2020-03-31 13:24:04 +0300

    interceptor for api

--- src/app/models/urls.ts
+++ src/app/models/urls.ts
@@ -1,6 +1,6 @@
 import {environment} from "../../environments/environment";
 
-const serverUrl = environment.serverUrl + "api/v1";
+const serverUrl = environment.serverUrl + "/api/v1";
 export const snpsInfoUrl = serverUrl + "/snps";
 export const searchOptionsUrl = (tfOrcl: "tf" | "cl") => serverUrl + "/search/" + tfOrcl + "/hint";
 export const searchSnpsResultsUrl = serverUrl + "/search/snps";
