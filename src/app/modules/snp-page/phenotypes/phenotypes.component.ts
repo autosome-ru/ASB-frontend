72c9de567170dc6c9521c2d7c107e679f8d9427d
serj <aswq22013@gmail.com>
2020-04-01 11:59:17 +0300

    empty content template

--- src/app/modules/snp-page/phenotypes/phenotypes.component.ts
+++ src/app/modules/snp-page/phenotypes/phenotypes.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {ChangeDetectionStrategy, Component, Input, OnInit} from "@angular/core";
 import {SnpInfoModel} from "../../../models/data.model";
 
 @Component({
@@ -11,19 +11,13 @@ export class PhenotypesComponent implements OnInit {
 
     @Input()
     public snpData: SnpInfoModel;
-    @Output()
-    public phenotypesEmpty = new EventEmitter<boolean>();
+
     public phenotypesDb: string[];
     constructor() { }
 
     ngOnInit(): void {
         this.phenotypesDb = Object.keys(this.snpData.phenotypes).filter(
             s => this.snpData.phenotypes[s].length > 0);
-        if (this.phenotypesDb.length === 0) {
-            this.phenotypesEmpty.emit(true);
-        } else {
-            this.phenotypesEmpty.emit(false);
-        }
     }
 
 }
