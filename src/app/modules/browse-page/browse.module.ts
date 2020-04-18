31235028dece68360f11339bd2be7500665901d5
wish1 <aswq22013@gmail.com>
2020-04-19 01:24:19 +0300

    fixes with lazy loading

--- src/app/modules/browse-page/browse.module.ts
+++ src/app/modules/browse-page/browse.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 
 import {BrowsePageComponent} from "./browse-page.component";
+import {AsbBrowsePageRoutingModule} from "./browse-page-routing.module";
 
 @NgModule({
     imports: [
         CommonModule,
+        AsbBrowsePageRoutingModule
     ],
     declarations: [BrowsePageComponent],
 })
