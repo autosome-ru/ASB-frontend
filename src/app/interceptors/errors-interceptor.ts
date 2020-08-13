66870c602927f5e9a131b5219b74acb316d78d8f
wish1 <aswq22013@gmail.com>
2020-08-13 21:53:08 +0300

    angular 10

--- src/app/interceptors/errors-interceptor.ts
+++ src/app/interceptors/errors-interceptor.ts
@@ -1,23 +1,25 @@
-import {Injectable} from "@angular/core";
+import {Inject, Injectable, PLATFORM_ID} from "@angular/core";
 import {HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpEvent} from "@angular/common/http";
 
 import {catchError} from "rxjs/operators";
 import {Observable, throwError} from "rxjs";
 import {ToastrService} from "ngx-toastr";
 import {Router} from "@angular/router";
+import {isPlatformBrowser} from "@angular/common";
 
 @Injectable()
 export class ErrorsInterceptor implements HttpInterceptor {
-
+    isBrowser: boolean
     constructor(
         private router: Router,
         private toastr: ToastrService,
-    ) {}
+        @Inject(PLATFORM_ID) private platformId
+    ) { this.isBrowser = isPlatformBrowser(platformId);}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(
             catchError(error => {
-                if (error instanceof HttpErrorResponse && error.status !== 404) {
+                if (error instanceof HttpErrorResponse && error.status !== 404 && this.isBrowser) {
                     this.toastr.error(
                         `Database lookup failed. Please try again later.`,
                         `${error.statusText} ${error.status}`,
