928b380b859e40a87aa01dd6a5c0a1aa6c774f4c
wish1 <aswq22013@gmail.com>
2020-09-03 01:57:50 +0300

    top menu fixed

--- src/app/modules/helpers/layout-template/layout.module.ts
+++ src/app/modules/helpers/layout-template/layout.module.ts
@@ -4,12 +4,16 @@ import {RouterModule} from "@angular/router";
 import {LayoutComponent} from "./layout.component";
 import {AsbFooterComponent} from "./footer/footer.component";
 import {AsbHeaderComponent} from "./header/header.component";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
+        MatMenuModule,
+        MatListModule,
 
     ],
     declarations: [
