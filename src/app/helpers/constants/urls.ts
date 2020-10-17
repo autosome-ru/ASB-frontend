e27a7f3bc483662fb54f40ea4dcae5ca4ac65ad0
wish1 <aswq22013@gmail.com>
2020-10-17 20:15:25 +0300

    search fix

--- src/app/helpers/constants/urls.ts
+++ src/app/helpers/constants/urls.ts
@@ -4,6 +4,6 @@ const serverUrl = environment.serverUrl + `/api/${recentRelease.api}`;
 
 export const browseUrl = serverUrl + "/browse";
 export const snpsInfoUrl = serverUrl + "/snps";
-export const searchOptionsUrl = (tfOrcl: "tf" | "cl") => serverUrl + "/search/" + tfOrcl + "/hint";
+export const searchOptionsUrl = (tfOrCl: "tf" | "cl") => serverUrl + "/search/" + tfOrCl + "/hint";
 export const searchOptionsByGeneNameUrl = serverUrl + "/search/gene_name/hint";
 export const searchSnpsResultsUrl = serverUrl + "/search/snps";
