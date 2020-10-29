f87097e3b1b31debdbbf724ee06929474a572927
wish1 <aswq22013@gmail.com>
2020-10-30 00:16:13 +0300

    fix

--- src/app/helpers/converter/snp-model.converter.ts
+++ src/app/helpers/converter/snp-model.converter.ts
@@ -110,8 +110,9 @@ function convertSnpModel(model: Partial<SnpGenPosBackendModel>):
     result.rsId = "rs" + model.rs_id;
     result.refBase = model.ref;
     result.altBase = model.alt;
-    if (model.context.length !== 49) {
+    if (model.context && model.context.length !== 49) {
         console.warn("Wrong context value", model.context.length);
+        result.context = ''
     } else {
         result.context = model.context.slice(0, 24) +
             `[${model.ref}/${model.alt}]` + model.context.slice(25);
