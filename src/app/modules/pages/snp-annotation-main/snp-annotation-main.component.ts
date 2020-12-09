066d45b31ebc0a15960dfe27a9f1db8bb7340c8a
m1kss <boytsovs.av@phystech.edu>
2020-12-09 19:32:05 +0300

    start tour

--- src/app/modules/pages/snp-annotation-main/snp-annotation-main.component.ts
+++ src/app/modules/pages/snp-annotation-main/snp-annotation-main.component.ts
@@ -4,16 +4,19 @@ import {ActivatedRoute} from "@angular/router";
 import {SeoModel} from "../../../models/seo.model";
 
 @Component({
-  selector: 'astra-snp-annotation-main',
-  templateUrl: './snp-annotation-main.component.html',
-  styleUrls: ['./snp-annotation-main.component.less'],
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'astra-snp-annotation-main',
+    templateUrl: './snp-annotation-main.component.html',
+    styleUrls: ['./snp-annotation-main.component.less'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SnpAnnotationMainComponent implements OnInit {
-  constructor(private seoService: SeoService, private route: ActivatedRoute) { }
+    steps: string[] = ['text-input', 'file-drop', 'examples'];
 
-  ngOnInit(): void {
-      this.seoService.updateSeoInfo(this.route.snapshot.data as SeoModel)
-  }
+    constructor(private seoService: SeoService, private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.seoService.updateSeoInfo(this.route.snapshot.data as SeoModel);
+    }
 }
--- src/app/modules/pages/snp-annotation-main/snp-annotation-main.module.ts
+++ src/app/modules/pages/snp-annotation-main/snp-annotation-main.module.ts
@@ -13,6 +13,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatIconModule} from '@angular/material/icon';
 import {AsbTourModule} from "../../shared/tour-template/tour-module";
 import {MatTooltipModule} from "@angular/material/tooltip";
+import {JoyrideModule} from "ngx-joyride";
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import {MatTooltipModule} from "@angular/material/tooltip";
         MatProgressBarModule,
         MatIconModule,
         AsbTourModule,
-        MatTooltipModule
+        MatTooltipModule,
+        JoyrideModule
     ]
 })
 export class SnpAnnotationMainModule { }
