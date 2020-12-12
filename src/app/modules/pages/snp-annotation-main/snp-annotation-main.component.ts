a509cd8a9185ce7353fdaf69fc74f32ee803e3d8
wish1 <aswq22013@gmail.com>
2020-12-13 01:49:46 +0300

    ananas

--- src/app/modules/pages/snp-annotation-main/snp-annotation-main.component.ts
+++ src/app/modules/pages/snp-annotation-main/snp-annotation-main.component.ts
@@ -1,4 +1,12 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+    ViewEncapsulation
+} from '@angular/core';
 import {SeoService} from "../../../services/seo.servise";
 import {ActivatedRoute} from "@angular/router";
 import {SeoModel} from "../../../models/seo.model";
@@ -10,15 +18,33 @@ import {SeoModel} from "../../../models/seo.model";
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SnpAnnotationMainComponent implements OnInit {
+export class SnpAnnotationMainComponent implements OnInit, OnDestroy {
+    @ViewChild('logo')
+    private logo: ElementRef<HTMLImageElement>;
     public steps: string[] = ['text-input', 'file-drop', 'examples', 'job'];
     text: string = 'This web service annotates a given list of SNPs with allele-specific binding events across' +
         ' a wide range of transcription factors and cell types\nusing ADASTRA that is constructed through the meta-analysis of more than 15000 ChIP-Seq experiments.';
+    private timerId: number;
 
     constructor(private seoService: SeoService, private route: ActivatedRoute) {
     }
 
     ngOnInit(): void {
         this.seoService.updateSeoInfo(this.route.snapshot.data as SeoModel);
+    }
+
+    ngOnDestroy() {
+        window.clearTimeout(this.timerId)
+    }
+
+    logoClicked() {
+        if (!this.logo.nativeElement.classList.contains('gear')) {
+            this.logo.nativeElement.classList.add('gear')
+            this.timerId = window.setTimeout(() => {
+                this.logo.nativeElement.classList.remove('gear')
+                window.clearTimeout(this.timerId)
+            }, 3000)
+        }
+
     }
 }
