7bcd9c47dbba1fe4915b94c70b0eb20f83fb8728
wish1 <aswq22013@gmail.com>
2021-02-18 20:06:20 +0300

    changes in ananastra pt.5

--- src/app/functions/scientific.helper.ts
+++ src/app/functions/scientific.helper.ts
@@ -2,11 +2,15 @@ export function writeScientificNum(num: number, precision: number): string {
   if (num === null) {
       return  `<span>n/a</span>`
   }
-  const power = Math.ceil(num);
+  let power = Math.ceil(num);
   const realNum = Math.pow(10, -num);
   if (num <= 2) {
     return `<span>${realNum.toFixed(precision)}</span>`;
   }
-  const base = (realNum * Math.pow(10, power)).toFixed(precision - 1);
+  let base = (realNum * Math.pow(10, power)).toFixed(precision - 1);
+  if (base === '10.'.padEnd(precision + 2, '0')) {
+      base = '1.'.padEnd(precision + 1, '0')
+      power = power - 1
+  }
   return `<span>${base}·10<sup>-${power}</sup></span>`;
 }
