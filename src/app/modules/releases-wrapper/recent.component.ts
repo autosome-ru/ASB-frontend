1d269df36a71df2ac51f4029b81197e4a998307b
Sergey Abramov <aswq22013@gmail.com>
2022-05-21 01:15:53 +0300

    query params fix

--- src/app/modules/releases-wrapper/recent.component.ts
+++ src/app/modules/releases-wrapper/recent.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from "@angular/core";
 import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Component({
     selector: "asb-recent",
@@ -8,17 +9,17 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
     encapsulation: ViewEncapsulation.None
 })
 export class RecentComponent implements OnInit {
-    public queryParams: Params;
+    public queryParams$: Observable<Params>;
 
     constructor(private route: ActivatedRoute, private router: Router) { }
 
     ngOnInit(): void {
-        this.queryParams = this.route.snapshot.queryParams
+        this.queryParams$ = this.route.queryParams
     }
 
     dismiss() {
         this.router.navigate([], {
-            queryParams: {...this.queryParams, releaseName: null},
+            queryParams: {...this.route.snapshot.queryParams, releaseName: null},
             queryParamsHandling: 'merge'
         })
     }
