c467833d000d14149edaaab3b89d8c7d668d2e8d
Sergey Abramov <aswq22013@gmail.com>
2022-07-14 22:04:31 +0300

    added tf-cl archive

--- src/app/helpers/constants/releases.ts
+++ src/app/helpers/constants/releases.ts
@@ -7,7 +7,7 @@ export const releasesList: ReleaseModel[] = [
         majorVersion: 5,
         date: "2022.05.19",
         releaseType: 'recent',
-        version: "v5.1.0",
+        version: "v5.1.1",
         size: '746.4MB',
         api: "v5",
         defaultFdrThreshold: '0.05',
